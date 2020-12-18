7786c235a90dc1098d07e97aabe8f28afe9a6c85
Dylan K <50585244+DylanK46@users.noreply.github.com>
2020-12-18 00:35:47 +0000

    added datastructurereader
    added datastructurereader
    tried to start settings conversion

---
+++ main/main.py
@@ -0,0 +1,24 @@
+import yaml
+
+def formdatastructure():
+    #takes in the data from /main/sampleinput.yaml
+    sampleinputfile = open("main/sampleinput.yaml", "r")
+
+    #declares the datastructure in <dict> format using a global;
+    #parses the yaml into dict
+    global datastructure
+    datastructure = (yaml.safe_load(sampleinputfile.read()))
+
+formdatastructure()
+
+
+class write:
+
+    #def init():
+
+    def settings():
+        sampleoutputfile = open("sampleoutput.yaml", "w")
+        sampleoutputfile.write("Woops! I have deleted the content!")
+        sampleoutputfile.close()
+
+write.settings()
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-Welcome to HomerDash.
+## Welcome to HomerDash.
 
-HomerDash aims to convert your YAML from Homer into Dashmachine Yaml, saving hours of re-writing.
+HomerDash aims to convert your YAML from Homer into Dashmachine YAML, saving hours of re-writing.
